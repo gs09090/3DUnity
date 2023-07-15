446ef460ef8b41de882d01dac08df5d5afade98a
gs09090 <gs09090@ajou.ac.kr>
2023-07-15 21:48:20 +0900

    PlayerJump
    플레이어 점프

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,31 +8,70 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
+
+    bool isJump;
 
     Vector3 moveVec;
 
+    Rigidbody rigid;
     Animator anim;
 
     void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
 
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
+    }
 
+    void Turn()
+    {
         // LookAt() : 지정된 벡터를 향해서 회전시켜주는 함수 
         transform.LookAt(transform.position + moveVec); // 나아가는 방향으로 바라본다
     }
 
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
+    }
 }
